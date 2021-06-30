61940598603b653f833eeea67f9a7b5ad6805341
Gabriel J. Suarez <ginnlaiho@gmail.com>
2021-06-30 18:10:58 -0500

    - Finish credit Scene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -16,7 +16,11 @@ export default class CreditsScene extends Phaser.Scene {
     }
 
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Gabriel Suárez', { fontSize: '26px', fill: '#fff' });
+    this.createdText = this.add.text(0, 0, 'Created With: Phaser 3', { fontSize: '26px', fill: '#fff' });
+    this.baseDesignText = this.add.text(0, 0, 'Base Design By: Phaser 3 Library', { fontSize: '26px', fill: '#fff' });
+    this.assetsText = this.add.text(0, 0, 'Sound Effects: Phaser 3 Library', { fontSize: '26px', fill: '#fff' });
+    this.musicText = this.add.text(0, 0, 'Music: Dafunk - Hardcore Power (DMCA FREE)', { fontSize: '26px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
     Phaser.Display.Align.In.Center(
@@ -29,7 +33,31 @@ export default class CreditsScene extends Phaser.Scene {
       this.zone
     );
 
+    Phaser.Display.Align.In.Center(
+        this.createdText,
+        this.zone
+    );
+
+    Phaser.Display.Align.In.Center(
+        this.baseDesignText,
+        this.zone
+    );
+
+    Phaser.Display.Align.In.Center(
+        this.assetsText,
+        this.zone
+    );
+
+    Phaser.Display.Align.In.Center(
+        this.musicText,
+        this.zone
+    );
+
     this.madeByText.setY(1000);
+    this.createdText.setY(1200);
+    this.baseDesignText.setY(1400);
+    this.assetsText.setY(1600);
+    this.musicText.setY(1800);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
@@ -48,6 +76,54 @@ export default class CreditsScene extends Phaser.Scene {
       delay: 1000,
       onComplete: function () {
         this.madeByTween.destroy;
+        this.scene.start('Title');
+      }.bind(this)
+    });
+
+    this.createdTextTweens = this.tweens.add({
+      targets: this.createdText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 2000,
+      onComplete: function () {
+        this.createdText.destroy;
+        this.scene.start('Title');
+      }.bind(this)
+    });
+
+    this.baseDesignTextTweens = this.tweens.add({
+      targets: this.baseDesignText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 3000,
+      onComplete: function () {
+        this.baseDesignText.destroy;
+        this.scene.start('Title');
+      }.bind(this)
+    });
+
+    this.assetsTextTweens = this.tweens.add({
+      targets: this.assetsText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 4000,
+      onComplete: function () {
+        this.assetsText.destroy;
+        this.scene.start('Title');
+      }.bind(this)
+    });
+
+    this.musicTextTweens = this.tweens.add({
+      targets: this.musicText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 5000,
+      onComplete: function () {
+        this.musicText.destroy;
         this.scene.start('Title');
       }.bind(this)
     });
